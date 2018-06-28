34691d433a5ff05e693934f90c497f691c0ec34b
Sebastien DIAZ <sebastien.diaz@gmail.com>
2018-06-28 11:08:09 +0200

    Update pushRSS.py

--- pushRSS.py
+++ pushRSS.py
@@ -2,10 +2,6 @@ import yaml
 import urllib
 import urllib.request as urllib2
 #create the object, assign it to a variable
-
-#set http_proxy=pcyvipncp2n.edf.fr:3134
-#set https_proxy=pcyvipncp2n.edf.fr:3134
-
 with open("listRSS.yaml") as stream:
     listRSS=yaml.load(stream)
     print ("load")
