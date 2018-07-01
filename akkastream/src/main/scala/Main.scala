ae368a2bb70039f728a953c5ac12277fd0759671
Sebastien DIAZ <sebastien.diaz@gmail.com>
2018-07-01 09:57:01 +0200

    start scala proof

--- akkastream/src/main/scala/Main.scala
+++ akkastream/src/main/scala/Main.scala
@@ -8,10 +8,22 @@ import akka.util.ByteString
 import scala.concurrent._
 import scala.concurrent.duration._
 import java.nio.file.Paths
-
+import scala.collection.mutable._
 
 
 object Main extends App {
+  private def getPipelineSource: Source[String, Future[IOResult]] = {
+    FileIO.fromPath(Paths.get("../content.rss"))
+      .via(Framing.delimiter(ByteString("\n"), 32000000)
+        .map(_.utf8String)
+      )
+  }
+  val stopWord: HashSet[String] = HashSet()
+  val filename = "stopWord.txt"
+  for (line <- scala.io.Source.fromFile(filename).getLines) {
+    stopWord += line
+  }
+
   println("Hello, World!")
   
   implicit val system = ActorSystem("QuickStart")
@@ -21,6 +33,24 @@ object Main extends App {
   
 
   val done: Future[Done] = source.runForeach(i ⇒ println(i))(materializer)
+ 
+
   implicit val ec = system.dispatcher
-  done.onComplete(_ ⇒ system.terminate())
+  //done.onComplete(_ ⇒ system.terminate())
+ def getOutput: Future[Done] = {
+    getPipelineSource
+      .map(_.toString)
+      .log("Before CSV Handler")
+      .map(_.replaceAll("<[^>]*>", ""))
+      .map( line =>
+        Source(line.split(" +").toList)
+        .map(_.replaceAll("\\p{C}|\\s+|\\r$|\\\\t|\\\\n|\\\\r", "").trim)
+        .filterNot(stopWord.contains(_))
+        .filterNot(_.contains("http"))
+        .filterNot(_.isEmpty).runWith(Sink.seq)
+      )
+      .runForeach(x => println(x))(materializer)
+  }
+  getOutput.onComplete(_ ⇒ system.terminate())
+
 }
